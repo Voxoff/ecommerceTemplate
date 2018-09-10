3e60985f6f5e827c8c91bc44c9d8828c3659c7c8
Voxoff <guybennettjones@hotmail.co.uk>
2018-09-10 12:19:22 +0100

    added devise and users

---
+++ db/migrate/20180910111254_add_price_to_product.rb
@@ -0,0 +1,5 @@
+class AddPriceToProduct < ActiveRecord::Migration[5.2]
+  def change
+    add_column :products, :price, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_09_10_111757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "products", force: :cascade do |t|
+    t.string "sku"
+    t.string "name"
+    t.string "description"
+    t.boolean "availability"
+    t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "price"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "product_id"
+    t.bigint "user_id"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "reviews", "products"
+  add_foreign_key "reviews", "users"
 end
