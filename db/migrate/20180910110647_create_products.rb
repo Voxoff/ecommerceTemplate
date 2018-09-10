e4932abcda51f29a82deca5607dbaceb9c06db16
Voxoff <guybennettjones@hotmail.co.uk>
2018-09-10 12:07:15 +0100

    controllers and models

---
+++ db/migrate/20180910110647_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :sku
+      t.string :name
+      t.string :description
+      t.boolean :availability
+      t.string :photo_url
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20180910110659_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.references :product, foreign_key: true
+      t.references :user, foreign_key: true
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
