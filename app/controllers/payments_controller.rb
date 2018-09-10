4eff8bae53583e164cd28d83ef5e5cb406cf98c7
Voxoff <guybennettjones@hotmail.co.uk>
2018-09-10 14:36:40 +0100

    orderforms

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,7 +5,24 @@ class PaymentsController < ApplicationController
   end
 
   def create
-    # ...
+    customer = Stripe::Customer.create(
+      source: params[:stripeToken],
+      email:  params[:stripeEmail]
+    )
+
+    charge = Stripe::Charge.create(
+      customer:     customer.id,   # You should store this customer id and re-use it.
+      amount:       @order.amount_cents,
+      description:  "Payment for product #{@order.teddy_sku} for order #{@order.id}",
+      currency:     @order.amount.currency
+    )
+
+    @order.update(payment: charge.to_json, state: 'paid')
+    redirect_to order_path(@order)
+
+  rescue Stripe::CardError => e
+    flash[:alert] = e.message
+    redirect_to new_order_payment_path(@order)
   end
 
   private
