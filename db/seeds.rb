e6fc15238fdcda5895c8be1b7f1f6b8fa9be8a04
Gonzalo Diaz <info@ventra.com.co>
2018-09-14 15:57:48 +0100

    added home page but cloudinary image tag not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,6 @@ Product.destroy_all
 puts 'Creating products...'
 
 Product.create!(price: 1, sku: 'jean-mimi', name: 'Jean-Michel - Le ', remote_photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large')
-Product.create!(price: 1, sku: 'octocat',   name: 'Octocat -  GitHub',      remote_photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
+Product.create!(price: 3, sku: 'octocat',   name: 'Octocat -  GitHub',      remote_photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
+Product.create!(price: 2, sku: 'product-three',   name: 'Product Three -  Facebook',      remote_photo_url: 'https://cdn-ak.f.st-hatena.com/images/fotolife/s/suzumidokoro/20160413/20160413220730.jpg')
 puts 'Finished!'
