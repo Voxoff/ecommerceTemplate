9c25556a409248da7c2fcd0f50967605ee8cec83
Voxoff <guybennettjones@hotmail.co.uk>
2018-10-11 12:20:11 +0100

    bootstrap accordion all done, bugs fixed and all

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,8 @@
 //= require rails-ujs
 //= require ./cable
 //= require jquery  
-//= require bootstrap
-//= require bootstrap-select
-//= require bootstrap/alert
-//= require bootstrap/dropdown
-//= require bootstrap-sprockets
+// require bootstrap
+// require bootstrap-sprockets
 // require rails.validations
 // require rails.validations.simple_form
 // We cannot use tree . since this would require activeadmin everywhere.
